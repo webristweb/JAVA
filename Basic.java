8f6e9ba0e5650c6a838238fd2d5d6402e759dbfa
webristweb <abhishekraj5508@gmail.com>
2024-06-13 00:35:16 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class Hello {
+    public static void main(Strings args[]){
+        Scanner sc = new Scanner (System.in);
+       // System.out.println("Hello World");
+        
+        //Out put Concept
+
+      /*  System.out.println("*");
+       System.out.println("**");
+       System.out.println("***");
+       System.out.println("****");
+        */
+
+        // Variable Concept
+
+         /*  int a = 10;
+        int b = 25;
+        int sum = a + b;
+        System.out.println("The Sum of two no is : " +sum);
+         */
+
+         //input Concept
+
+       /*  int a = sc.nextInt();
+       int b = sc.nextInt();
+       int sum = a+b;
+       System.out.println(sum);
+       */
+
+       // Input Concept
+       Strings name = sc.nextLine();
+       System.out.println(name);
+    }
+}
