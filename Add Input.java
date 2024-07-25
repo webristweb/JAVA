97173a360ef129e23b2bdc8952b7c078bfda8f84
Abhishek Raj <abhishekraj5508@gmail.com>
2024-07-25 09:09:24 +0530

    Add files via upload

---
+++ Add Input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class multiplication {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Numbers");
+        int a = sc.nextInt();
+        System.out.println("Enter Second  Numbers");
+        int b = sc.nextInt();
+        int c = a + b;
+       System.out.println("multiplication =" + c);
+    }
+}
